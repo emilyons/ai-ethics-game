1a0458f5471a143d91c93bbb36422b74ae17d3fa
ekerby <emilykerby@gmail.com>
2024-08-21 22:25:51 -0700

    add nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import "./Nav.scss";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul className="nav__list">
+        <li>
+          <NavLink className="nav__link" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="nav__link" to="about">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="nav__link" to="resources">
+            Resources
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
