adbaf23498d6387f2760d46d74d93ab609f75731
ekerby <emilykerby@gmail.com>
2024-08-21 22:27:31 -0700

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import Nav from "../Nav/Nav";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <Link className="logo" to="/">
+        AI Guardians: Adventures in Ethics
+      </Link>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
