f2c2f8c193aa114f91b491a5884dadace923b170
ekerby <emilykerby@gmail.com>
2024-10-03 22:00:08 -0700

    add Home page starter page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const HomePage = () => {
     return (
-        <main>
-            <h1>AI Guardians</h1>
-            <p>Ready to Play?</p>
-        </main>
+        <div>
+        <h1>Welcome to AI Guardians!</h1>
+        <p>Explore the world of AI ethics and help the Guardians save Techville.</p>
+        <button>
+          <a href="/chapter1">Start Chapter 1</a>
+        </button>
+      </div>
     )
 }
 
