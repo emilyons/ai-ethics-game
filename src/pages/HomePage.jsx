640e4820ba28b32c574e2c63c65c394f8a6f1a5a
ekerby <emilykerby@gmail.com>
2024-10-03 22:34:18 -0700

    add font

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ const HomePage = () => {
     return (
         <main>
             <h1>AI Guardians</h1>
+            <p>Explore the world of AI ethics and help the Guardians save Techville.</p>
             <p>Ready to Play?</p>
             <button>
                 <a href="/chapter1">Start Chapter 1</a>
